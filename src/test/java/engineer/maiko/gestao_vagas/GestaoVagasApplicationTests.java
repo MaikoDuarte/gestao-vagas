221340cbe265167a7c43401b8fc01a5781c68924
Maiko Duarte <mkdu185@gmail.com>
2023-10-27 16:44:06 -0300

    start: Initial Commit

---
+++ src/test/java/engineer/maiko/gestao_vagas/GestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package engineer.maiko.gestao_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
