3602b849326a20fbdf02fc37afc906eb63221e15
Maiko Duarte <mkdu185@gmail.com>
2023-11-04 16:49:23 -0300

    feat: Add Company controller

---
+++ src/main/java/engineer/maiko/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -0,0 +1,38 @@
+package engineer.maiko.gestao_vagas.modules.company.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import engineer.maiko.gestao_vagas.modules.company.entities.CompanyEntity;
+import engineer.maiko.gestao_vagas.modules.company.useCases.CreateCompanyUseCase;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+
+  @Autowired
+  private CreateCompanyUseCase createCompanyUseCase;
+
+  @PostMapping("/")
+ public ResponseEntity<Object> create(@Valid @RequestBody CompanyEntity companyEntity)  {
+  try {
+    var result = this.createCompanyUseCase.execute(companyEntity);
+    return ResponseEntity.ok().body(result);
+
+  }catch(Exception e) {
+    e.printStackTrace();
+    return ResponseEntity.badRequest().body(e.getMessage());
+
+
+ }
+
+}
+}
+
+  
+
