93aeb754efe7937391dbfad1b564c0dd85a13539
Maiko Duarte <mkdu185@gmail.com>
2023-11-04 16:34:03 -0300

    feat: Add Candidate repository

---
+++ src/main/java/engineer/maiko/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -0,0 +1,13 @@
+package engineer.maiko.gestao_vagas.modules.candidate;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+  Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
+
+
+  
+}
