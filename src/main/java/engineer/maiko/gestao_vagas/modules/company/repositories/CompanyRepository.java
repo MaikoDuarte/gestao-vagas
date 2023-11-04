7ab68af03dc51cb93e55f3995223c2d103f6c307
Maiko Duarte <mkdu185@gmail.com>
2023-11-04 16:47:28 -0300

    feat: Add Company repository

---
+++ src/main/java/engineer/maiko/gestao_vagas/modules/company/repositories/CompanyRepository.java
@@ -0,0 +1,15 @@
+package engineer.maiko.gestao_vagas.modules.company.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import engineer.maiko.gestao_vagas.modules.company.entities.CompanyEntity;
+
+
+public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
+    Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
+    Optional<CompanyEntity> findByUsername(String username);
+  
+}
