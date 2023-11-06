48ae6c49363b0fa2f1ab4f59e05ed93d8198dc9b
Maiko Duarte <mkdu185@gmail.com>
2023-11-06 15:52:43 -0300

    feat: Create Company DTOs

---
+++ src/main/java/engineer/maiko/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -0,0 +1,13 @@
+package engineer.maiko.gestao_vagas.modules.company.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateJobDTO {
+
+  private String description;
+  private String benefits;
+  private String level;
+
+  
+}
