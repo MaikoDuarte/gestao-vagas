fc5ee1a1fa90d4bc0825f46f3cec225e9e0f10d0
Maiko Duarte <mkdu185@gmail.com>
2023-11-06 15:43:29 -0300

    feat: Create Company entities with JPA and Lombok

---
+++ src/main/java/engineer/maiko/gestao_vagas/modules/company/entities/JobEntity.java
@@ -0,0 +1,49 @@
+package engineer.maiko.gestao_vagas.modules.company.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "job")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class JobEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+  private String description;
+  private String benefits;
+
+  @NotBlank(message = "Esse campo é obrigatório")
+  private String level;
+  
+
+  @ManyToOne()
+  @JoinColumn(name = "company_id", insertable = false, updatable = false)
+  private CompanyEntity companyEntity;
+
+  @Column(name = "company_id", nullable = false)
+  private UUID companyId;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+  
+  
+}
