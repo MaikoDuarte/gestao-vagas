086dedef6124417c0933e6b99a656170695fb195
Maiko Duarte <mkdu185@gmail.com>
2023-11-24 18:24:20 -0300

    feat: Add Job and User not found exception

---
+++ src/main/java/engineer/maiko/gestao_vagas/exceptions/JobNotFoundException.java
@@ -0,0 +1,8 @@
+package engineer.maiko.gestao_vagas.exceptions;
+
+public class JobNotFoundException extends RuntimeException {
+  public JobNotFoundException() {
+    super("Job not found");
+  }
+  
+}
