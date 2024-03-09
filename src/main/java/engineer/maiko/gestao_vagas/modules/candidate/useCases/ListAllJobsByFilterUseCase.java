a7d2283305cd1d12eba35da316ebf7c767715e6b
Maiko Duarte <mkdu185@gmail.com>
2024-03-09 15:50:32 -0300

    Updated multiple use cases

---
+++ src/main/java/engineer/maiko/gestao_vagas/modules/candidate/useCases/ListAllJobsByFilterUseCase.java
@@ -0,0 +1,23 @@
+package engineer.maiko.gestao_vagas.modules.candidate.useCases;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import engineer.maiko.gestao_vagas.modules.company.entities.JobEntity;
+import engineer.maiko.gestao_vagas.modules.company.repositories.JobRepository;
+
+@Service
+public class ListAllJobsByFilterUseCase {
+
+  @Autowired
+  private JobRepository jobRepository;
+
+  public List<JobEntity> execute(String filter) {
+    return this.jobRepository.findByDescriptionContainingIgnoreCase(filter);
+
+
+  }
+  
+}
--- src/main/java/engineer/maiko/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
+++ src/main/java/engineer/maiko/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
@@ -3,7 +3,9 @@ package engineer.maiko.gestao_vagas.modules.company.useCases;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import engineer.maiko.gestao_vagas.exceptions.CompanyNotFoundException;
 import engineer.maiko.gestao_vagas.modules.company.entities.JobEntity;
+import engineer.maiko.gestao_vagas.modules.company.repositories.CompanyRepository;
 import engineer.maiko.gestao_vagas.modules.company.repositories.JobRepository;
 
 @Service
@@ -11,9 +13,16 @@ public class CreateJobUseCase {
   
   @Autowired
   private JobRepository jobRepository;
+
+  @Autowired
+  private CompanyRepository companyRepository;
   
+  @SuppressWarnings("null")
   public JobEntity execute(JobEntity jobEntity) {
-  
+
+    companyRepository.findById(jobEntity.getCompanyId()).orElseThrow(() -> {
+      throw new CompanyNotFoundException();
+    });
       return this.jobRepository.save(jobEntity);
     }
 
